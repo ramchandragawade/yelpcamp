9bab36f29ba502f7eadfb7652c64f93ef2e4b33f
ramchandragawade <raajgawade39@gmail.com>
2023-12-31 03:40:25 +0530

    checking error logs

--- app.js
+++ app.js
@@ -142,6 +142,7 @@ app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message)
         err.message = 'Something went wrong';
+    console.log(err);
     res.status(statusCode).render('error', { err });
 });
 
